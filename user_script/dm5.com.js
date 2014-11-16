fcd2d571aa2fb2404b8408a308bc1d10d225d7fd
dishuostec <dishuostec@gmail.com>
2014-11-16 17:37:50 +0800

    添加用户脚本

---
+++ user_script/dm5.com.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name         书签新漫画提醒
+// @namespace    http://your.homepage/
+// @version      0.1
+// @description  高亮显示新的未读漫画
+// @author       dishuostec
+// @match        http://*.dm5.com/bookmarker/
+// @grant        none
+// ==/UserScript==
+
+$(function(){
+    $('#divBookmarkers').find('ul').each(function(){
+        var $this = $(this);
+        var $li = $this.find('li');
+        var $new = $li.eq(1).find('a').last();
+        var $last = $li.eq(3).find('a').first();
+
+        if ($new.attr('href') !== $last.attr('href')) {
+            $this.css({
+                color: '#fff',
+                background: '#f88'
+            }).find('a').css('color', '#fff');
+        } else {
+            $this.css({
+                background: '#888'
+            });
+        }
+    });
+});
